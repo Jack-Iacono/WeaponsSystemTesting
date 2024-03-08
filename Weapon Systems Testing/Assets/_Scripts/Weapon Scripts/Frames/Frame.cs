c4412b195d51fa164cc951b4878160bf1397d016
John (Jack) Iacono <90567244+Jack-Iacono@users.noreply.github.com>
2024-03-08 18:40:39 -0500

    Began Implementing the Frame Behavior Tree

--- Weapon Systems Testing/Assets/_Scripts/Weapon Scripts/Frames/Frame.cs
+++ Weapon Systems Testing/Assets/_Scripts/Weapon Scripts/Frames/Frame.cs
@@ -8,7 +8,25 @@ public abstract class Frame : ScriptableObject
     public FrameStats baseStats;
     public FrameStats currentStats { get; private set; }
 
+    protected TimerManager timerManager = new TimerManager();
+
     public Weapon connectedWeapon { get; private set; }
+
+    #region Behavior Tree
+
+    protected Node.Status status = Node.Status.RUNNING;
+
+    public Tree frameBehavior = new Tree("Frame Behavior");
+
+    protected Sequence activateActions;
+    protected Sequence refillActions;
+    protected Sequence readyActions;
+
+    protected const string activateSequenceKey = "Activate";
+    protected const string refillSequenceKey = "Refill";
+    protected const string readySequenceKey = "Ready";
+
+    #endregion
 
     public abstract bool Activate();
     public virtual void Initialize(Weapon connectedWeapon)
@@ -17,11 +35,50 @@ public abstract class Frame : ScriptableObject
 
         // TEMPORARY: Change later for weapon crafting
         currentStats = baseStats;
+
+        timerManager.Add(activateSequenceKey, new Timer(currentStats.useTime, ActivateTimerCallback));
     }
     public virtual void UpdateFrame()
     {
+        //controls the behavior of the method
+        if (status == Node.Status.RUNNING)
+        {
+            status = frameBehavior.Check();
+        }
+        else if (status == Node.Status.SUCCESS)
+        {
+            switch (frameBehavior.GetCurrentSequenceName())
+            {
+                default:
+                    frameBehavior.StartSequence(readySequenceKey);
+                    break;
+            }
 
+            status = frameBehavior.Check();
+        }
     }
+
+    #region Firing Behaviors
+
+    protected virtual Node.Status BeginActivate() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status UpdateActivate() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status EndActivate() { return Node.Status.SUCCESS; }
+
+    protected virtual Node.Status BeginRefill() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status UpdateRefill() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status EndRefill() { return Node.Status.SUCCESS; }
+
+    protected virtual Node.Status BeginReady() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status UpdateReady() { return Node.Status.SUCCESS; }
+    protected virtual Node.Status EndReady() { return Node.Status.SUCCESS; }
+
+    #endregion
+
+    #region Timer Callbacks
+
+    protected virtual void ActivateTimerCallback(string key) { }
+
+    #endregion
 }
 public abstract class RangedFrame : Frame
 {
@@ -29,8 +86,6 @@ public abstract class RangedFrame : Frame
 
     public override bool Activate()
     {
-        Debug.Log(currentStats.useTime);
-
         // this functionality stays here in case of usePrimaryAmmo
         if (currentStats.ready && currentStats.currentAmmo >= currentStats.ammoPerShot)
         {
